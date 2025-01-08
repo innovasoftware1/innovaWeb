6576b7e935bb2f1e34974dc3fb0ece98f9d62673
innovasoftware1 <gerencia@innovapublicidad.com.co>
2025-01-08 09:27:56 -0500

    Version 5.0.2

---
+++ assets/js/routes.js
@@ -0,0 +1,16 @@
+// Base URL de tu proyecto
+const BASE_URL = "file:///C:/Proyectos%20INNOVA/innovaWeb/";
+
+// Definición de rutas
+const ROUTES = {
+  INDEX: `${BASE_URL}index.html`,
+  PRODUCTOS: {
+    VALLAS: {
+      METALICAS_TUBULARES: `${BASE_URL}pageProduct/tubulares.html`,
+    },
+  },
+  CONTACTO: `${BASE_URL}contacto.html`,
+};
+
+// Exportar las rutas
+export default ROUTES;
